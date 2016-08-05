7abd5bc720713fe94da2509b22f29fd48f46581b
Alexander Milevski <info@w8r.name>
2016-08-05 18:57:15 +0200

    avl

--- src/tree.js
+++ src/tree.js
@@ -1,8 +1,5 @@
 import Node from './node';
 
-const RED   = true;
-const BLACK = false;
-
 export default class Tree {
 
   /**
@@ -24,519 +21,271 @@ export default class Tree {
     if (!node) {
       return 0;
     } else {
-      return 1 + Math.max(this.depth(node.left), this.depth(node.right));
+      return 1 + Math.max(this.depth(node.left), depth(root!.right));
     }
   }
-
-  /**
-   * In order traversal
-   */
-  forEach (callback, node = this.root) {
-    if (this.root === null) {
-      throw new Error('The tree is empty');
-    }
-
-    if (node) {
-      this.forEach(callback, node.left);
-      callback(node);
-      this.forEach(callback, node.right);
-    }
-  }
-
-
-  // Balancing
-
-
-  /**
-   *   Let Q be P's right child.
-   * Set P's right child to be Q's left child.
-   * [Set Q's left-child's parent to P]
-   *   Set Q's left child to be P.
-   *   [Set P's parent to Q]
-   */
-  leftRotate (node) {
-    let newRoot = node.right;
-    node.right = newRoot.left;
-    if (newRoot.left !== null) {
-      newRoot.left.parent = node;
-    }
-    newRoot.parent = node.parent;
-
-    if (node.parent === null) {
-      this.root = newRoot;
-    } else if (node === node.parent.left) {
-      node.parent.left = newRoot;
+  //return the max depth of the tree
+  private func depth(rooty: RBTNode?) -> Int {
+    if rooty == nil {
+      return 0
     } else {
-      node.parent.right = newRoot;
+      return 1+(max(depth(root!.left), depth(root!.right)))
     }
-    newRoot.left = node;
-    node.parent = newRoot;
   }
 
-
-  /**
-   * Let P be Q's left child.
-   *   Set Q's left child to be P's right child.
-   *   [Set P's right-child's parent to Q]
-   *   Set P's right child to be Q.
-   *   [Set Q's parent to P]
-   */
-  rightRotate (node) {
-    let newRoot = node.left;
-    node.left = newRoot.right
-    if (newRoot.right !== null) {
-      newRoot.right.parent = node;
-    }
-
-    newRoot.parent = node.parent;
-    if (node.parent === null) {
-      root = newRoot;
-    } else if (node === node.parent.right) {
-      node.parent.right = newRoot;
-    } else {
-      node.parent.left = newRoot;
-    }
-    newRoot.right = node;
-    node.parent = newRoot;
+  public func inOrder() {
+    inOrder(root)
   }
-
+  //Prints the in order traversal of the current tree
+  private func inOrder(root: RBTNode?) {
+    if self.root == nil {
+      print("The tree is empty.")
+    }
+    if root == nil {
+      return
+    }
+    inOrder(root!.left)
+    print("Data: \(root!.data) Color: \(root!.color)")
+    inOrder(root!.right)
+  }
 
   // instertion
 
-  _insertFixup (key) {
-    let inserted = this.find(key)
-    return this._insert1(inserted);
-  }
-
   /**
-   * There's only root
-   * @param {Node} inserted
-   */
-  _insert1 (inserted) {
-    if (this.root === inserted) {
-      this.root.color = BLACK;
-    }
-    return this._insert2(inserted);
-  }
-
-
-  /**
-   * Case for inserting a node as a child of a black node
-   * @param  {Node} inserted
+   * @param  {*}    key
+   * @param  {*=}   data
    * @return {Node|null}
    */
-  _insert2 (inserted) {
-    if (inserted.parent.color === BLACK) {
-      return inserted;
+  insert (key, data = null) {
+    let inserted;
+    if (this.root) {
+      inserted = this._insert(key, data, root);
+    } else {
+      inserted = this.root = new Node(key, data);
     }
-    return this._insert3(inserted);
+    this.size++;
+    return inserted;
   }
 
 
   /**
-   * Insert case for if the parent is black and parent's siblng is black
-   * @param  {Node} inserted
+   * @param  {*}    key
+   * @param  {*=}   data
+   * @param  {Node} subtree
    * @return {Node|null}
    */
-  _insert3 (inserted) {
-    let parent = inserted.parent;
-    if (parent.sibling() !== null &&
-        parent.sibling().color === RED) {
-      parent.color = BLACK;
-      parent.sibling().color = BLACK;
-      let g = inserted.grandparent();
-      g.color = RED;
-      if (g.parent === null) {
-        g.color = BLACK;
-      }
-    }
-    return this._insert4(inserted);
-  }
-
-
-  /**
-   * Insert case for Node N is left of parent and parent is right of grandparent
-   * @param  {Node} insert
-   * @return {Node|null}
-   */
-  _insert4 (inserted) {
-    var inserted = insert
-    if (inserted === inserted.parent.right &&
-      inserted.grandparent().left === inserted.parent) {
-      this.leftRotate(inserted.parent)
-      inserted = inserted.left;
-    } else if (inserted === inserted.parent.left &&
-      inserted.grandparent().right === inserted.parent) {
-      this.rightRotate(inserted.parent)
-      inserted = inserted.right;
-    }
-    return this._insert5(inserted);
-  }
-
-
-  /**
-   * Insert case for Node n where parent is red and parent's sibling is black
-   * @param  {Node} insert
-   * @return {Node|null}
-   */
-  _insert5 (inserted) {
-    let parent = inserted.parent;
-    let parentSibling = parent.sibling();
-    if (parent.color === RED &&
-        parentSibling === null ||
-        parentSibling.color === BLACK) {
-      let grandparent = inserted.grandparent();
-      if (inserted === parent.left &&
-          grandparent.left === parent) {
-        inserted.parent.color = BLACK;
-        grandparent.color = RED;
-        if (inserted === parent.left) {
-          this.rightRotate(grandparent);
-        }
-      } else if (inserted === parent.right &&
-                 grandparent().right === parent) {
-        parent.color = BLACK;
-        grandparent.color = RED;
-        this.leftRotate(grandparent);
-      }
-    }
-  }
-
-
-  /**
-   * @param  {*}  key
-   * @param  {*=} value
-   */
-  insert(key, value) {
-    this.insertAtNode(key, value, root)
-    this.insertFixup(key);
-  }
-
-
-  /**
-   * Basic BST insert implementation
-   * @param  {*}  key
-   * @param  {*=} value
-   * @param  {*}  root
-   */
-  insertAtNode(key, value, root) {
-    if (this.root === null) {
-      this.root = new Node(key, value);
-      return this.root;
-    } else if (this.comparator(key, root.key) < 0) {
-      let left = root.left;
-      if (left) {
-        return this.insertAtNode(key, value, left);
+  _insert(key, data, subtree) {
+    var cmp = this.comparator(key, subtree.key);
+    if (cmp < 0) {
+      if (subtree.left) {
+        return this._insert(key, data, subtree.left);
       } else {
-        parent.left = new Node(key, value);
-        parent.left.parent = root;
-        return parent.left;
+        let child = new Node(key, data, null, null, subtree, 1);
+        subtree.left = child;
+        this.balance(child);
+        return child;
       }
     } else {
-      let right = root.right;
-      if (right) {
-        return this.insertAtNode(key, value, right);
+      if (subtree.right) {
+        return this._insert(key, data, subtree.right);
       } else {
-        root.right = new Node(key, value);
-        root.right.parent = root;
-        return parent.right;
+        let child = new Node(key, data, null, null, subtree, 1);
+        subtree.right = child;
+        this.balance(child);
+        return child;
+      }
+    }
+  }
+
+  // Balancing tree
+
+  /**
+   * @param  {Node} node
+   */
+  updateHeightUpwards (node) {
+    const lHeight = node.left  ? node.left.height  : 0;
+    const rHeight = node.right ? node.right.height : 0;
+    node.height = Math.max(lHeight, rHeight) + 1;
+    this.updateHeightUpwards(node.parent);
+  }
+
+
+  /**
+   * @param  {Node} node
+   * @return {Number}
+   */
+  lrDifference (node){
+    const lHeight = node.left  ? node.left.height  : 0;
+    const rHeight = node.right ? node.right.height : 0;
+    return lHeight - rHeight;
+  }
+
+
+  /**
+   * @param  {Node} node
+   */
+  balance (node) {
+    if (!node) return;
+
+    this.updateHeightUpwards(node.left);
+    thid.updateHeightUpwards(node.right)
+
+    let node0, node1, node2;
+    let subtree0, subtree1, subtree2, subtree3;
+    let parent   = node.parent;
+
+    const lrFactor = this.lrDifference(node);
+    if (lrFactor > 1) {
+      // left-left or left-right
+      if (this.lrDifference(node.left) > 0) {
+        // left-left
+        node0 = node;
+        node2 = node.left;
+        node1 = node.left ? node.left.left : null;
+
+        subtree0 = node1 ? node1.left  : null;
+        subtree1 = node1 ? node1.right : null;
+        subtree2 = node2 ? node2.right : null;
+        subtree3 = node.right;
+      } else {
+        // left-right
+        node0 = node
+        node1 = node.left
+        node2 = node.left ? node.left.right : null;
+
+        subtree0 = node1 ? node1.left  : null;
+        subtree1 = node2 ? node2.left  : null;
+        subtree2 = node2 ? node2.right : null;
+        subtree3 = node.right;
+      }
+    } else if (lrFactor < -1) {
+      // right-left or right-right
+      if (this.lrDifference(node.right) < 0) {
+        // right-right
+        node1 = node;
+        node2 = node.right;
+        node0 = node2 ? node2.right : null;
+
+        subtree0 = node.left
+        subtree1 = node2 ? node2.left  : null;
+        subtree2 = node0 ? node0.left  : null;
+        subtree3 = node0 ? node0.right : null;
+      } else {
+        // right-left
+        node1 = node;
+        node0 = node.right;
+        node2 = node0 ? node0.left : null;
+
+        subtree0 = node.left;
+        subtree1 = node2 ? node2.left  : null;
+        subtree2 = node2 ? node2.right : null;
+        subtree3 = node0 ? node0.right : null;
+      }
+    } else {
+      // Don't need to balance 'node', go for parent
+      return this.balance(node.parent);
+    }
+
+    // node2 is always the head
+
+    if (node.isRoot()) {
+      this.root = node2;
+      this.root.parent = null;
+    } else if (node.isleft()) {
+      if (parent) parent.left  = node2;
+      if (node2) node2.parent = parent;
+    } else if (node.isright()) {
+      if (parent) parent.right = node2;
+      if (node2) node2.parent = parent;
+    }
+
+    if (node2) node2.left  = node1;
+    if (node2) node2.right = node0;
+    if (node1) node1.parent     = node2;
+    if (node0) node0.parent     = node2;
+
+    if (node1) node1.left    = subtree0;
+    if (node1) node1.right   = subtree1;
+    if (subtree0) subtree0.parent = node1;
+    if (subtree1) subtree1.parent = node1;
+
+    if (node0) node0.left    = subtree2;
+    if (node0) node0.right   = subtree3;
+    if (subtree2) subtree2.parent = node0;
+    if (subtree3) subtree3.parent = node0;
+
+    this.updateHeightUpwards(node1);    // Update height from left
+    this.updateHeightUpwards(node0);    // Update height from right
+
+    if (node2) this.balance(node2.parent);
+  }
+
+  // Remove nodes
+
+  remove(key) {
+    const node = this.search(key, this.root);
+    if (node) {
+      this._remove(node);
+      this.size--;
+      return true;
+    }
+    return false;
+  }
+
+
+  _remove(node) {
+    if (node.isLeaf()) { // remove and balance up
+      let parent = node.parent;
+      if (parent) {
+        if (node.isleft() + node.isright() === 0) {
+          throw new Error('Tree is invalid');
+        }
+
+        if (node.isleft()) {
+          parent.left = null;
+        } else if (node.isright()) {
+          parent.right = null;
+        }
+
+        this.balance(parent);
+      } else { // at root
+        this.root = null;
+      }
+      return true;
+    } else { // Handle stem cases
+      let replacement = node.left ? node.left.max() : null;
+      if (replacement === node) replacement = null;
+      if (replacement) {
+        node.key  = replacement.key;
+        node.data = replacement.data;
+        this._remove(replacement);
+        return true;
+      }
+
+      replacement = node.right ? node.right.min() : null;
+      if (replacement === node) replacement = null;
+      if (replacement) {
+        node.key = replacement.key;
+        node.data = replacement.data;
+        this._remove(replacement);
+      }
+    }
+    return false;
+  }
+
+
+  search(key, subtree) {
+    subtree = subtree || root;
+    if (subtree) {
+      var cmp = this.comparator(ket, subtree.key);
+      if (cmp === 0) {
+        return node;
+      } else if (cmp < 0) {
+        return this.search(key, subtree.left);
+      } else if (cmp > 0) {
+        return this.search(key, subtree.right);
       }
     }
     return null;
-  }
-
-
-  // Search
-
-
-  /**
-   * @param  {*} key
-   * @param  {Node=} root
-   * @return {Node|null}
-   */
-  find (key, root = this.root) {
-    const cmp = this.comparator(key, root.key);
-    if (cmp === 0) return root;
-
-    if (root.key !== key && root.right === null && root.left == null) {
-      return null;
-    } else if (cmp > 0) {
-      return this.find(key, root.right);
-    } else if (cmp > 0) {
-      return this.find(key, root.left);
-    } else return null;
-  }
-
-
-  // Removal
-
-
-  /**
-   * Transplant the positions of two nodes in the RBTree
-   * @param  {Node} n1
-   * @param  {Node} n2
-   */
-  replaceNode (n1, n2) {
-    let key   = n1.key;
-    let color = n1.color;
-
-    n1.key   = n2.key;
-    n1.color = n2.color;
-
-    n2.key   = key;
-    n2.color = color;
-  }
-
-
-  /**
-   * Returns the node with the minimum value in the subtree
-   * @param  {Node=} node
-   * @return {Node}
-   */
-  min (node = this.root) {
-    let minimumNode = node;
-    while (minimumNode.left !== null) {
-      minimumNode = minimumNode.left;
-    }
-    return minimumNode;
-  }
-
-
-  /**
-   * Returns the next largest node in the tree
-   * @param  {Node=} node
-   * @return {Node}
-   */
-  successor(node = this.root) {
-    let nextLargestNode = node;
-    if (nextLargestNode.right !== null) {
-      return this.min(nextLargestNode.right);
-    }
-
-    let successor = nextLargestNode.parent;
-    while (successor !== null && nextLargestNode === successor.right) {
-      nextLargestNode = successor;
-      successor = successor.parent;
-    }
-    return successor
-  }
-
-
-  /**
-   * Returns the next smallest node in the tree
-   * @param  {Node=} node
-   * @return {Node}
-   */
-  predecessor(node = this.root) {
-    let nextSmallestNode = node;
-    if (nextSmallestNode.left !== null) {
-      return this.min(nextSmallestNode.left);
-    }
-
-    let successor = nextSmallestNode.parent;
-    while (successor !== null && nextSmallestNode === successor.left) {
-      nextSmallestNode = successor;
-      successor = successor.parent;
-    }
-    return successor;
-  }
-
-
-  /**
-   * Returns the node with the largest value in the subtree
-   * @param  {Node=} node
-   * @return {Node}
-   */
-  max (node) {
-    let rootNode = node;
-    while (rootNode.right !== null) {
-      rootNode = rootNode.right;
-    }
-    return rootNode;
-  }
-
-
-  /**
-   * call to remove a node from the tree
-   * @param  {*} key
-   * @return {Node|null}
-   */
-  remove(key) {
-    let toRemove = this.find(key);
-    return toRemove ? this.removeNode(toRemove) : toRemove;
-  }
-
-
-  /**
-   * @param  {Node} node
-   * @return {Node|null}
-   */
-  removeNode(node) {
-    // case for if node is the only node in the tree
-    if (node.left   === null &&
-        node.right  === null &&
-        node.parent === null) {
-      self.root = null;
-      return node;
-    }
-
-    //case for if node is a red node w/o children
-    if (node.left === null &&
-        node.right === null && node.color === RED) {
-      if (node.parent.left === node) {
-        node.parent.left = null;
-      } else {
-        node.parent.right = null;
-      }
-      return node;
-    }
-
-    // node has two children
-    if (node.left !== null && node.right !== null) {
-      let prev = this.max(node.left);
-      node.key = prev.key;
-      node = prev;
-      node = prev;
-    }
-
-    //case for toDel having one child
-    let child = (node.right === null) ? node.left : node.right;
-    if (node.color === BLACK && child !== null) {
-      node.color = child.color;
-      this.remove1(node);
-    }
-
-    if (child !== null) {
-      this.replaceNode(node, child);
-      if (node.parent === null && child !== null) {
-        child.color = BLACK;
-      }
-    }
-
-    if (node.parent.left === node) {
-      node.parent.left = null;
-      return node;
-    } else if (node.parent === null) {
-      return;
-    } else {
-      node.parent.right = null;
-    }
-  }
-
-
-  /**
-   * Delete case for if parent is null after deletion
-   * @param  {Node} node
-   * @return {Node|null}
-   */
-  remove1 (node) {
-    return (node.parent === null) ? node : this.remove2(node);
-  }
-
-  /**
-   * Case to fix tree after deletion and sibling is red
-   * @param  {Node} node
-   * @return {Node|null}
-   */
-  remove2 (node) {
-    let sibling = node.sibling();
-    if (sibling.color === RED) {
-      node.parent.color = RED;
-      sibling.color = BLACK;
-      if (node === node.parent.left) {
-         this.leftRotate(node.parent);
-      } else {
-         this.rightRotate(node.parent);
-      }
-    }
-    return this.remove3(node);
-  }
-
-
-  /**
-   * Fixing tree when parent is black and
-   * sibling is black and sibling's children are also black
-   * @param  {Node} node
-   * @return {Node|null}
-   */
-  remove3 (node) {
-    let sibline = node.sibling();
-    if (node.parent.color   === BLACK &&
-        sibling.color       === BLACK &&
-        sibling.left.color  === BLACK &&
-        sibling.right.color === BLACK) {
-
-      sibling.color = RED;
-      node.parent.color = BLACK;
-    } else {
-      this.remove4(node);
-    }
-    return node;
-  }
-
-
-  /**
-   * @param  {Node} node
-   * @return {Node|null}
-   */
-  remove4 (node) {
-    let sibling = node.sibling();
-    if (node.parent.color  === RED &&
-       sibling.color       === BLACK &&
-       sibling.left.color  === BLACK &&
-       sibling.right.color === BLACK) {
-
-      sibling.color = RED;
-      node.parent.color = BLACK;
-    } else {
-      this.remove5(node);
-    }
-    return node;
-  }
-
-
-  /**
-   * @param  {Node} node
-   * @return {Node}
-   */
-  remove5 (node) {
-    let sibling = node.sibling();
-    if (node === node.parent.left &&
-        sibling.color       === BLACK &&
-        sibling.left.color  === RED &&
-        sibling.right.color === BLACK) {
-
-      sibling.color = RED;
-      sibling.left.color = BLACK;
-      this.rightRotate(sibling);
-    } else if (node === node.parent.right &&
-               sibling.color       === BLACK &&
-               sibling.left.color  === BLACK &&
-               sibling.right.color === RED) {
-
-      sibling.color = RED;
-      sibling.right.color = BLACK;
-      this.leftRotate(sibling);
-    }
-  }
-
-
-  /**
-   * @param  {Node} node
-   * @return {Node}
-   */
-  remove6 (node) {
-    let color = node.sibling().color;
-    let parent = node.parent;
-    let sibling = node.sibling();
-    parent.color = BLACK;
-    if (node === parent.left) {
-      sibling.right.color = BLACK;
-      this.leftRotate(parent);
-    } else {
-      sibling.left.color = BLACK;
-      this.rightRotate(parent);
-    }
-    return node;
   }
 }
